aacb9675d22d1d76c01393ca18b06dfb8e580444
JulianVic <josejulianvicenteperez@gmail.com>
2023-06-30 11:40:16 -0600

    Borre actus

--- financiera-cvm/backend/helpers/generarID.js
+++
@@ -1,8 +0,0 @@
-const generarIDUnica = () => {
-    const min = 10000;
-    const max = 99999;
-    const id = Math.floor(Math.random() * (max - min)) + min;
-    return id;
-}
-
-export default generarIDUnica;
--- financiera-cvm/backend/routes/clientaRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { mostrarClientas, registrarClienta }  from "../controllers/clientaController.js";
-
-const router = express.Router();
-
-router.get("/", mostrarClientas);
-router.post("/registrarClienta", registrarClienta);
-
-export default router;
